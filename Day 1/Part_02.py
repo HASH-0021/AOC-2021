ff08a9de92d06ff51ad722fc082196eee55643a1
HASH-0021 <dvsriharsha2198@gmail.com>
2021-12-13 16:02:45 +0530

    Solution to Day 1 puzzle

---
+++ Day 1/Part_02.py
@@ -0,0 +1,38 @@
+
+class NegativeIntegerException(Exception):
+	'''Exception raised for custom purpose.
+	
+	Attributes:
+		message -- explanation of the exception
+	'''
+	def __init__(self, message):
+		self.message = message
+
+count,i = 0,0
+try:
+	with open('Depths.txt', 'r') as my_file:
+		depths = my_file.read().split('\n')
+	for enum,each_depth in enumerate(depths):
+		depths[enum] = int(each_depth)
+		if depths[enum] < 0:
+			raise NegativeIntegerException("Atleast one of the depth value is less than zero..")
+			break
+	if not len(depths):
+		print("Please enter some measurements in 'Depths.txt'..")
+	elif len(depths) == 1:
+		print("N/A - no previous measurement found..")
+	else:
+		windowed_depths = list()
+		while i <= len(depths)-3:
+			windowed_depths.append(depths[i] + depths[i+1] + depths[i+2])
+			i += 1
+		i = 0
+		while i < len(windowed_depths)-1:
+			if windowed_depths[i+1] - windowed_depths[i] > 0:
+				count += 1
+			i += 1
+		print(f"There are {count} measurements that are larger than the previous measurement.")
+except ValueError:
+	print("Please enter only whole numbers for depths..")
+except NegativeIntegerException as nie:
+	print(f"Exception Message: {nie.message}")
